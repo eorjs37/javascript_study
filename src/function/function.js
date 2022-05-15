9983265f34c38adba19581098a4e289e01585621
CHOI DAE GEON <chleorjs37@gmail.com>
2022-05-15 21:41:44 +0900

    커밋

--- src/function/function.js
+++ src/function/function.js
@@ -5,3 +5,19 @@ var square = function(number){
 
 const returnSquare = square(2);
 console.log(returnSquare);
+
+
+//2.함수 선언문 정의
+function add(x,y){
+    return x+y;
+}
+
+console.log(add(2,5));
+
+//3.화살표 함수
+const arrow = (x,y) =>{
+    return x+y;
+}
+
+
+console.log(arrow(1,2));
---
+++ src/function/hosting.js
@@ -0,0 +1,10 @@
+//호이스팅
+console.dir(hosting);
+console.dir(hosting2);
+function hosting(){
+    console.log('hosting');
+}
+
+var hosting2 = () =>{
+    console.log('hosting2');
+}
